cf1d16c12631d2a50a293adbcda918c73be01a2b
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-11-17 02:52:30 +0100

    correction date convertion utils function

--- bot/lib/utils.js
+++ bot/lib/utils.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 module.exports = {
 	saveJSON: function saveJSON(filepath, data) {
 		data = JSON.stringify(data);
@@ -15,9 +17,13 @@ module.exports = {
 
 	frenchDateToNumber: function frenchDateToNumber(date) {
 		var months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+		var hasToConvert = _.some(months, function(month) { return date.indexOf(month) !== -1; });
+		if (!hasToConvert) return date;
+
+		var oldDate = date;
 		date = date.replace(/\n/g, '').trim();
 		date = date.split(' ');
-		date[1] = (_(months).indexOf(date[1])+1).toString();
+		date[1] = (months.indexOf(date[1])+1).toString();
 		date[1] = (date[1]).toString().length > 1 ? date[1] : "0" + date[1];
 		date[0] = (date[0]).toString().length > 1 ? date[0] : "0" + date[0];
 		date = date.reverse().join('-');
