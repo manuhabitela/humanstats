4be59910fa18cfb82c6f9ba7e5975e1d299ebb6d
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-11-28 00:39:44 +0100

    styling cities links to better identify selected cities

--- scripts/views/cities.js
+++ scripts/views/cities.js
@@ -2,9 +2,9 @@ define(["jquery", "backbone", "underscore"], function($, Backbone, _) {
 	var CitiesView = Backbone.View.extend({
 		template: _.template([
 			'<ul class="inline-list">',
-				'<li><a href="#">Tout</a></li>',
+				'<li><a href="#" data-color="#000">Tout</a></li>',
 				'<% _.each(cities, function(city) { %>',
-				'<li><a href="#" data-id="<%- city.id %>" style="background-color: <%- city.color %>"><%- city.name %></a></li>',
+				'<li><a href="#" data-id="<%- city.id %>" data-color="<%- city.color %>"><%- city.name %></a></li>',
 				'<% }) %>',
 			'</ul>'].join('')
 		),
@@ -24,6 +24,8 @@ define(["jquery", "backbone", "underscore"], function($, Backbone, _) {
 		toggleCity: function(e) {
 			var $city = $(e.currentTarget);
 			var cityId = $city.attr('data-id');
+			var that = this;
+
 			if (cityId) {
 				this.collection.toggle(cityId);
 				$city.toggleClass('active', this.collection.isActive(cityId));
@@ -31,6 +33,14 @@ define(["jquery", "backbone", "underscore"], function($, Backbone, _) {
 				this.collection.deactivateAll();
 				this.$('a').removeClass('active');
 			}
+
+			this.$('a').each(function(n, link) {
+				var $link = $(link);
+				if ($link.hasClass('active') || (!$link.attr('data-id') && !that.$('a.active').length))
+					$link.css({ 'color': 'white', 'background-color': $link.attr('data-color') });
+				else
+					$link.css({ 'color': $link.attr('data-color'), 'background-color': '' });
+			});
 			e.preventDefault();
 		}
 	});
