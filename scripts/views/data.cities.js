d7eb24d4160154731914d5b469e2a40c018376ff
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-12-05 13:43:50 +0100

    map view: we init the map at first rendering this prevents the map to have 0 width/height when we init the map while its container is hidden

--- scripts/views/data.cities.js
+++ scripts/views/data.cities.js
@@ -8,7 +8,7 @@ define(["jquery", "backbone", "underscore", "d3", "topojson", "datamaps"], funct
 			_.bindAll(this, 'render');
 
 			var that = this;
-			var mapOptions = {
+			this.mapOptions = {
 				element: this.el,
 				fills: {
 					bubble: '#fff',
@@ -39,27 +39,32 @@ define(["jquery", "backbone", "underscore", "d3", "topojson", "datamaps"], funct
 			};
 			this.data.cities.each(function(city) {
 				if (city.get('color'))
-					mapOptions.fills[city.id] = city.get('color');
-			});
-			this.map = new Datamap(mapOptions);
+					this.mapOptions.fills[city.id] = city.get('color');
+			}, this);
 		},
 
 		render: function() {
+			if (!this.map)
+				this.map = new Datamap(this.mapOptions);
+
 			if (!this.data || !this.map.done) return false;
-			var sortedEventIds =  _(this.data.filtered.cities).chain().pluck('eventIds').sortBy(function(ids) { return ids.length*-1; }).value();
-			var maxEventsNb = sortedEventIds && sortedEventIds[0] ? sortedEventIds[0].length : 0;
-			var maxRadius = 40;
+
+			var maxAttendeesCount =  _(this.data.filtered.cities).chain().pluck('attendeesCount').max().value();
+			var radiusScale = d3.scale.linear().domain([1, maxAttendeesCount]).range([10, 40]);
+
 			var bubbles = _(this.data.filtered.cities).map(function(city) {
-				var radius = city.eventIds.length / maxEventsNb * maxRadius;
+				var radius = radiusScale(city.attendeesCount);
 				return _.extend({ latitude: city.coords.lat, longitude: city.coords.lng, radius: radius, fillKey: city.id }, city);
 			});
 			var dots = _(this.data.filtered.cities).map(function(city) {
 				return _.extend({ latitude: city.coords.lat, longitude: city.coords.lng, radius: 1, fillKey: 'dot' }, city);
 			});
-			this.map.bubbles([].concat(bubbles, dots), {
+
+			this.map.bubbles(bubbles.concat(dots), {
 				popupTemplate: function (geography, data) {
 					return ['<div class="hoverinfo"><strong>' +  data.name + '</strong>',
 					'<br/>' + data.eventIds.length + ' évènements',
+					'<br/>' + data.attendeesCount + ' participants venus ' + data.appearancesCount + ' fois',
 					'</div>'].join('');
 				},
 				borderWidth: 1,
